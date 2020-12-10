a0b14571e1ead1a2927d49f944407caac8a341f3
folsmore <daniilpastorov@mail.ru>
2020-12-10 16:38:31 +0300

    fixFunc

--- tBot/bot.php
+++ tBot/bot.php
@@ -3,18 +3,22 @@
     use Telegram\Bot\Api; 
     $token = '1482885814:AAFu3S6CTLyGK7n1g65psKligPN4nMQr2eQ';
     $telegram = new Api($token);
-    $result = $telegram -> getWebhookUpdates(); //данные о пользовательском сообщении
-
-    $text = $result["message"]["text"]; //Текст сообщения
-    $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+     //данные о пользовательском сообщении
+    global $result;
+    global $text;
+    global $chat_id;
     $name = $result["message"]["from"]["first_name"]; //Имя пользователя
     $surname = $result["message"]["from"]["last_name"]; //Фамилия пользователя
     $phone = $result["message"]["from"]["phone_number"]; //Телефон пользователя
-    $keyboard =[ [ [
-        'text'=>'Send contact',
-        'request_contact'=>true,
-    ]]];
-    function getTextMessages($text,$keyboard,$chat_id){
+    global $keyboard;
+    function getTextMessages($text,$keyboard,$chat_id,$result){
+        $result = $telegram -> getWebhookUpdates();
+        $text = $result["message"]["text"];
+        $chat_id = $result["message"]["chat"]["id"];
+        $keyboard =[ [ [
+            'text'=>'Send contact',
+            'request_contact'=>true,
+        ]]];
         if($text){
             if ($text == "/start") {
                 $reply = "Привет, отправь контакт";
